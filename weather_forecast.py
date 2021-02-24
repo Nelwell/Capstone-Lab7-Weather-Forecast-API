309345114d919d8c7f25ca4527c8b2ee34d9b4e5
Nelwell <nick.elwell206@gmail.com>
2021-02-24 06:05:12 -0600

    break code up into dedicated, focused functions, get forecadt data for wind speed and weather description, as well as print the 5 day forecast out in a clean format

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,20 +1,66 @@
 import os
 import requests
 from datetime import datetime
-from pprint import pprint
 
-key = os.environ.get('open_weather_key')  # environment variable
-query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': key}  # dictionary to store params
-
+wx_key = os.environ.get('open_weather_key')  # environment variable
+query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': wx_key}  # dictionary to store params
 url = 'http://api.openweathermap.org/data/2.5/forecast'  # base url
 
-data = requests.get(url, params=query).json()  # gets data in JSON format
-pprint(data)
 
-fcst_data_interval_list = data['list']
+def main():
+    location = get_location()
+    weather_data, error = get_wx_data(location, wx_key)
+    if error:
+        print('Sorry, could not get weather for that location or locations doesn\'t exist')
+    else:
+        display_wx_forecast(weather_data, location)
 
-for fcst in fcst_data_interval_list:
-    temp = fcst['main']['temp']
-    timestamp = fcst['dt']
-    valid_datetime = datetime.fromtimestamp(timestamp)
-    print(f'At {valid_datetime} the temperature will be {temp} °F')
+
+def get_location():
+    city, country = '', ''
+    while len(city) == 0:
+        city = input('Enter the name of the city: ').upper().strip()
+
+    while len(country) != 2 or not country.isalpha():
+        country = input('Enter a 2-letter country code: ').upper().strip()
+
+    location = f'{city},{country}'
+    return location
+
+
+def get_wx_data(location, wx_key):
+    try:
+        query = {'q': location, 'units': 'imperial', 'appid': wx_key}
+        response = requests.get(url, params=query)
+        response.raise_for_status()  # Raise exception for 400 or 500 errors
+        data = response.json()  # this may error too, if response is not JSON
+        print(data)
+        return data, None
+    except Exception as e:
+        print(e)
+        print(response.text)  # added for debugging, logging would be better
+        return None, e
+
+
+def display_wx_forecast(weather_data, location):
+    fcst_data_list = weather_data['list']
+
+    print(f'\n*** HERE\'S YOUR {location} 5-DAY FORECAST ***')
+    for element in fcst_data_list:
+        temp = element['main']['temp']
+        wind_spd = element['wind']['speed']
+        timestamp = element['dt']
+        # UTC time, we don't know where the user is at.
+        # Local time on their computer will make no sense for areas outside their time zone.
+        valid_datetime = datetime.utcfromtimestamp(timestamp)
+        print(f'\nAt {valid_datetime}UTC:\n'
+              f'\tTemperature: {temp}°F')
+        wx_desc_list = element['weather']
+        for wx in wx_desc_list:
+            wx_desc = wx['description']
+            print(f'\tWeather: {wx_desc}')
+        print(f'\tWind Speed: {wind_spd}mph')
+
+
+if __name__ == '__main__':
+    main()
